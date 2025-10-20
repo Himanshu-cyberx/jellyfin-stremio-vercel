d03eda440d2e639504e43e30c67e34fae462ac96
Himanshu-cyberx <76645404+Himanshu-cyberx@users.noreply.github.com>
2025-10-20 16:52:03 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  api: { bodyParser: { sizeLimit: '2mb' } },
+  reactStrictMode: true
 };
+
 module.exports = nextConfig;
